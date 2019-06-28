3a09b96e334e1a245a669e0dc48156270688d6f7
MARLENE APARECIDA ARGENTON <49505213+marleneargenton@users.noreply.github.com>
2019-06-27 21:38:47 -0300

    Add files via upload

--- Interaction_project.py
+++ Interaction_project.py
@@ -4,14 +4,15 @@
 #
 # Autora: Marlene Aparecida Argenton
 
-# Outro módulo, import as classes e contém algumas funções:
-# criam-se listas para bancos, firmas e invetidores:
-# Faz-se um loop utilizando n, i, append às listas, retorna
-# Outra função, define as estratégias dos investidores, que depositando recursos nos Bancos, quando a taxa de juros
-# alcança um parâmetro pré-definido, ou realizam retirada de seus recursos, caso identifiquem risco de liquidez das
-# instituições financeiras, ou oferecerem uma remuneração maior que a oferecida, normalmente, pelo mercado.
-# Por fim, a interação entre bancos e firmas, definida pela estratégias de firmas na busca de empréstimos, que a partir
-# de um determinado patamar de taxa optam por contrair o empréstimo.
+# No segundo módulo, são importadas as classes e definidas as funções para:
+#  criação das listas para Bancos, Firmas e Investidores:
+#  Faz-se um loop utilizando n, i, append às listas  e retorna
+#  define-se as estratégias dos investidores:
+#  depositam recursos nos Bancos, quando a taxa de juros alcança um parâmetro pré-definido, ou
+#  realizam retirada de seus recursos, caso identifiquem risco de liquidez das instituições financeiras, ao oferecerem
+#  uma remuneração maior que a oferecida, cujo parâmetro foi pré-definido, normalmente, pelo mercado.
+#  Por fim, a interação entre bancos e firmas, definida pela estratégias de firmas na busca de empréstimos, que a
+#  partir de um determinado patamar de taxa optam por contrair o empréstimo.
 
 
 import random
