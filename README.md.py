d3584f0fe8df824ad6aeeb0c52a5157a6c6e4b77
MARLENE APARECIDA ARGENTON <49505213+marleneargenton@users.noreply.github.com>
2019-06-27 23:12:38 -0300

    Add files via upload

---
+++ README.md.py
@@ -0,0 +1,20 @@
+### Python4ABMIpea  - Exercício 3
+#
+# Professor: Bernardo Furtado
+#
+# Autora: Marlene Aparecida Argenton
+#
+#
+# 1. Objetivo: criar agentes (bancos, firmas e investidores) e verificar as interações relativas
+# as alterações no valor das taxas de juros praticadas.
+# No primeiro modulo, são criadas as classes: Bancos, Firmas e Investidores.
+# No segundo módulo, são importadas as classes e definidas as funções para:
+#   criação das listas para Bancos, Firmas e Investidores:
+#   Faz-se um loop utilizando n, i, append às listas  e retorna
+#   define-se as estratégias dos investidores:
+#   depositam recursos nos Bancos, quando a taxa de juros alcança um parâmetro pré-definido, ou
+#   realizam retirada de seus recursos, caso identifiquem risco de liquidez das instituições financeiras, ao oferecerem
+#   uma remuneração maior que a oferecida, cujo parâmetro foi pré-definido, normalmente, pelo mercado.
+#   Por fim, a interação entre bancos e firmas, definida pela estratégias de firmas na busca de empréstimos, que a
+#   partir de um determinado patamar de taxa optam por contrair o empréstimo.
+# No terceiro módulo, cria-se o gráfico que demonstra o efeito da taxa de juros na simulação de depósitos e saques.
