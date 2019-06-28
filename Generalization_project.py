3fa92463a62a245f6fd0469cddca95b21a9c86f8
MARLENE APARECIDA ARGENTON <49505213+marleneargenton@users.noreply.github.com>
2019-06-27 22:19:58 -0300

    Add files via upload

--- Generalization_project.py
+++ Generalization_project.py
@@ -14,21 +14,21 @@ def grafic(inv, ban, fir, rem, amount):
         os.remove(file)
     with open(file, 'a') as f:
         f.write('depósitos; retiradas; remuneração\n')
-        for i in range(len(inv)) and range(len(ban)):
-            inv, ban, fir, = Interaction_project.main(inv[i], ban[i], fir[i])
+        for i in range(len(inv)):
+            inv, bank, firm = Interaction_project.main(inv[i], ban[i], fir[i])
             ban1 = random.choice(ban)
             rem1 = random.choice(rem)
             amount1 = random.choice(amount)
             if rem1 > 11.52:
-                if ban[i].check_funds(amount1):
+                if ban1[i].check_funds(amount1):
                     ban1[i].withdraw(inv[i].withdraw(amount1))
             elif rem1 > 5.76:
                 if inv[i].check_funds(amount1):
-                    inv[i].deposit(ban[i].add_balance(amount1))
-            f.write('{};{};{};'.format(inv[i].deposit, inv[i].withdraw, rem1))
+                    inv[i].deposit(ban1[i].add_balance(amount1))
+            f.write('{};{};{};'.format(inv[i].balance, ban[i].balance, rem1))
             f.write('\n')
-            retlist.append(inv[i].deposit)
-            deplist.append(inv[i].withdraw)
+            retlist.append(inv[i].balance)
+            deplist.append(ban[i].balance)
             taxlist.append(rem1)
     # cria o gráfico
     plt.plot(deplist, taxlist, color='#17a589', label='depósitos')
@@ -39,9 +39,9 @@ def grafic(inv, ban, fir, rem, amount):
 
 
 if __name__ == '__main__':
-    po = [20, 40, 60, 80, 120, 250, 350, 400, 600, 800, 900, 1000]
-    ba = [3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30]
-    fi = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200]
-    re = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20]
-    am = [100, 200, 300, 400, 1000, 1500, 2000, 3000, 5000, 6500, 9000, 10000]
+    po = 20, 40, 60, 80, 120, 250, 350, 400, 600, 800, 900, 1000
+    ba = 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30
+    fi = 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200
+    re = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20
+    am = 100, 200, 300, 400, 1000, 1500, 2000, 3000, 5000, 6500, 9000, 10000
     grafic(po, ba, fi, re, am)
