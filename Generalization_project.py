c0d277a2b1e36c9fd8b5bd1bdeb9ac8317cf1dbf
MARLENE APARECIDA ARGENTON <49505213+marleneargenton@users.noreply.github.com>
2019-06-27 23:31:02 -0300

    Add files via upload

--- Generalization_project.py
+++ Generalization_project.py
@@ -1,3 +1,12 @@
+# Python4ABMIpea  - Exercício 3
+#
+# Professor: Bernardo Furtado
+#
+# Autora: Marlene Aparecida Argenton
+
+# Cria-se o gráfico que demonstra o efeito sobre os depósitos e saques de investidores, a partir da simulação da
+# alteração dos parâmetros da taxa de juros.
+
 import random
 import Interaction_project
 import os
@@ -9,39 +18,42 @@ def grafic(inv, ban, fir, rem, amount):
     # cria listas para os resultados
     retlist = []
     deplist = []
-    taxlist = []
+    taxlist1 = []
+    taxlist2 = []
     if os.path.exists(file):
         os.remove(file)
     with open(file, 'a') as f:
         f.write('depósitos; retiradas; remuneração\n')
         for i in range(len(inv)):
-            inv, bank, firm = Interaction_project.main(inv[i], ban[i], fir[i])
-            ban1 = random.choice(ban)
+            invlist, banklist, firmlist = Interaction_project.main(inv[i], ban[i], fir[i])
+            ban1 = random.choice(banklist)
             rem1 = random.choice(rem)
             amount1 = random.choice(amount)
             if rem1 > 11.52:
-                if ban1[i].check_funds(amount1):
-                    ban1[i].withdraw(inv[i].withdraw(amount1))
+                if ban1.check_funds(amount1) > 0:
+                    ban1.withdraw(invlist[i].withdraw(amount1))
+                    retlist.append(amount1)
+                    taxlist1.append(rem1)
             elif rem1 > 5.76:
-                if inv[i].check_funds(amount1):
-                    inv[i].deposit(ban1[i].add_balance(amount1))
-            f.write('{};{};{};'.format(inv[i].balance, ban[i].balance, rem1))
+                if invlist[i].check_funds(amount1) > 0:
+                    invlist[i].deposit(ban1.add_balance(amount1))
+                    deplist.append(amount1)
+                    taxlist2.append(rem1)
+            f.write('{};{};{};'.format(invlist[i].deposit, invlist[i].withdraw, rem1))
             f.write('\n')
-            retlist.append(inv[i].balance)
-            deplist.append(ban[i].balance)
-            taxlist.append(rem1)
     # cria o gráfico
-    plt.plot(deplist, taxlist, color='#17a589', label='depósitos')
-    plt.plot(retlist, taxlist, color='red', label='retiradas')
-    plt.legend(loc='right', shadow=True)
+    plt.plot(taxlist2, deplist, color='#17a589', label='depósitos')
+    plt.plot(taxlist1, retlist, color='red', label='retiradas')
+    plt.legend(loc='center', shadow=True)
     plt.xlabel('taxa de juros')
     plt.show()
+    return deplist, retlist
 
 
 if __name__ == '__main__':
-    po = 20, 40, 60, 80, 120, 250, 350, 400, 600, 800, 900, 1000
-    ba = 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30
-    fi = 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200
-    re = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20
-    am = 100, 200, 300, 400, 1000, 1500, 2000, 3000, 5000, 6500, 9000, 10000
-    grafic(po, ba, fi, re, am)
+    po = [20, 40, 60, 80, 120, 250, 350, 400, 600, 800, 900, 1000]
+    ba = [3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30]
+    fi = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200]
+    re = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20]
+    am = [1100, 2200, 3300, 4400, 6000, 8500, 10000, 13000, 15000, 16500, 19000, 21000]
+    dl, rl = grafic(po, ba, fi, re, am)
